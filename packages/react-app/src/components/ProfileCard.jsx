e9a2e7a8426cd7580c1b81b0b61f1aa7f5c824dc
Andrew Keddis <andrew.keddis@tma1.com>
2022-11-05 03:47:00 +0000

    add hardcoded grid data

--- packages/react-app/src/components/ProfileCard.jsx
+++ packages/react-app/src/components/ProfileCard.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
-function ProfileCard({imageUrl}) {
-  return <div className="profile-card">
-      <div className ="profile-img"></div>
-     <img src={imageUrl} alt="" width="50%" height=""/>
-  </div>
+function ProfileCard({ id, imageUrl, handle, address, name }) {
+  return (
+    <div className="profile-card">
+      <div className="mint-banner">MINT</div>
+      <div className="card-title">{name}</div>
+      <img className="profile-img" src={imageUrl} alt="" width="50%" height=""></img>
+      <div className="card-text">{handle}</div>
+      <div className="card-text">
+        {address.substr(0, 3)}...{address.substr(address.length - 3)}
+      </div>
+    </div>
+  );
 }
 
 export default ProfileCard;
--- packages/react-app/src/views/CardsGrid.jsx
+++ packages/react-app/src/views/CardsGrid.jsx
@@ -1,6 +1,5 @@
 import { ProfileCard } from "../components";
 
-
 /**
  * web3 props can be passed from '../App.jsx' into your local view component for use
  * @param {*} yourLocalBalance balance on current network
@@ -8,41 +7,76 @@ import { ProfileCard } from "../components";
  * @returns react component
  **/
 function CardsGrid() {
-  return<div className="cards-grid">
-    <ProfileCard 
-      imageUrl= 'https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U'
-    />
-    <ProfileCard 
-    imageUrl= 'https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U'
-    />
-    <ProfileCard 
-    imageUrl= 'https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U'
-    />
-    <ProfileCard 
-    imageUrl= 'https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U'
-    />
-    <ProfileCard 
-    imageUrl= 'https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U'
-    />
-    <ProfileCard 
-    imageUrl= 'https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U'
-    />
-    <ProfileCard 
-    imageUrl= 'https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U'
-    />
-    <ProfileCard 
-    imageUrl= 'https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U'
-    />
-    <ProfileCard 
-    imageUrl= 'https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U'
-    />
-    <ProfileCard 
-    imageUrl= 'https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U'
-    />
-    <ProfileCard 
-    imageUrl= 'https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U'
-    />
-  </div>
+  return (
+    <div className="cards-grid">
+      <ProfileCard
+        imageUrl="https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U"
+        handle="sasicodes.test"
+        address="0x01d79BcEaEaaDfb8fD2F2f53005289CFcF483464"
+        name="Sasi"
+      />
+      <ProfileCard
+        imageUrl="https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U"
+        handle="sasicodes.test"
+        address="0x01d79BcEaEaaDfb8fD2F2f53005289CFcF483464"
+        name="Sasi"
+      />
+      <ProfileCard
+        imageUrl="https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U"
+        handle="sasicodes.test"
+        address="0x01d79BcEaEaaDfb8fD2F2f53005289CFcF483464"
+        name="Sasi"
+      />
+      <ProfileCard
+        imageUrl="https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U"
+        handle="sasicodes.test"
+        address="0x01d79BcEaEaaDfb8fD2F2f53005289CFcF483464"
+        name="Sasi"
+      />
+      <ProfileCard
+        imageUrl="https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U"
+        handle="sasicodes.test"
+        address="0x01d79BcEaEaaDfb8fD2F2f53005289CFcF483464"
+        name="Sasi"
+      />
+      <ProfileCard
+        imageUrl="https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U"
+        handle="sasicodes.test"
+        address="0x01d79BcEaEaaDfb8fD2F2f53005289CFcF483464"
+        name="Sasi"
+      />
+      <ProfileCard
+        imageUrl="https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U"
+        handle="sasicodes.test"
+        address="0x01d79BcEaEaaDfb8fD2F2f53005289CFcF483464"
+        name="Sasi"
+      />
+      <ProfileCard
+        imageUrl="https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U"
+        handle="sasicodes.test"
+        address="0x01d79BcEaEaaDfb8fD2F2f53005289CFcF483464"
+        name="Sasi"
+      />
+      <ProfileCard
+        imageUrl="https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U"
+        handle="sasicodes.test"
+        address="0x01d79BcEaEaaDfb8fD2F2f53005289CFcF483464"
+        name="Sasi"
+      />
+      <ProfileCard
+        imageUrl="https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U"
+        handle="sasicodes.test"
+        address="0x01d79BcEaEaaDfb8fD2F2f53005289CFcF483464"
+        name="Sasi"
+      />
+      <ProfileCard
+        imageUrl="https://lens.infura-ipfs.io/ipfs/Qma8mXoeorvPqodDazf7xqARoFD394s1njkze7q1X4CK8U"
+        handle="sasicodes.test"
+        address="0x01d79BcEaEaaDfb8fD2F2f53005289CFcF483464"
+        name="Sasi"
+      />
+    </div>
+  );
 }
 
 export default CardsGrid;
